f30c48ea277e632a6568867f23957098095fcdb8
Geovanny Rojas Lindao <geovanny_rl14@hotmail.com>
2022-02-08 15:48:24 -0500

    Modificar la vista de clubes

--- src/app/backend/tasks/clubesAsos.service.ts
+++ src/app/backend/tasks/clubesAsos.service.ts
@@ -1,26 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { clubesAsos } from '../models/clubesAsos';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClubesService {
 
-  info:clubesAsos[] = [];
-
   constructor(private http: HttpClient) {
-    this.cargarInfo();
    }
 
    cargarInfo(){
-     return this.http.get("https://api.npoint.io/2d8fecd7a69f7a5eceb3").subscribe((res:any) => {
-       this.info = res
-     })
+     return this.http.get<any>("https://api.npoint.io/2d8fecd7a69f7a5eceb3")
    }
-
-  detalles() {
-    return this.http.get("https://api.npoint.io/2d8fecd7a69f7a5eceb3");
-  }
 
 }
